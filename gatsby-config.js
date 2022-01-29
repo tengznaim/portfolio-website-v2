4caf69fafa8d4e509b38baaad2aae87d9f4342ec
Tengku Naim <naimtmas@gmail.com>
2022-01-29 22:17:55 +0800

    registered svg plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,19 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Tengku Naim`,
+    // siteUrl: `https://www.gatsbyjs.com`, Update Later
+    description: `Tengku Naim is a computer science undergraduate who loves using data and computers to solve human problems.`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+  ],
 }
