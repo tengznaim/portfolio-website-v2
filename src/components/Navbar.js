d56d0f34eb952d642e2a024acab78446232c793f
Tengku Naim <naimtmas@gmail.com>
2022-01-31 22:13:47 +0800

    added navbar scroll animations and styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,46 @@
-import React from "react"
+import React, { useRef, useEffect, useState } from "react"
 import { Link } from "gatsby"
 import * as styles from "../styles/navbar.module.css"
 import PersonalLogo from "../assets/personal-logo.svg"
 import Sidebar from "./Sidebar"
 
 export default function Navbar() {
+  const scrollPos = useRef(0)
+  const [scrolledToTop, setScrolledTop] = useState(true)
+  const [navVisible, setNavVisible] = useState(true)
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+
+    function handleScroll() {
+      const currPos = document.body.getBoundingClientRect().top
+
+      // Scrolling down AND below threshold
+      if (currPos < scrollPos.current && currPos <= -70) {
+        setScrolledTop(false)
+        setNavVisible(false)
+        // Scrolling up AND above threshold
+      } else if (currPos > scrollPos.current && currPos > -70) {
+        setScrolledTop(true)
+        //Scrolling up but not above threshold
+      } else if (currPos > scrollPos.current) {
+        setNavVisible(true)
+      }
+      scrollPos.current = currPos
+    }
+
+    //Cleanup Function
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [scrolledToTop, navVisible])
+
   return (
-    <header className={styles.header}>
+    <header
+      className={`${styles.header} ${!navVisible && styles.hidden} ${
+        !scrolledToTop && styles.glass
+      }`}
+    >
       <nav className={styles.navbar}>
         <PersonalLogo id={styles.logo} />
         <div className={styles.linksContainer}>
