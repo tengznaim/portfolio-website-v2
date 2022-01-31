e0a9fd76562890e1ba9117ee567d06092c167033
Tengku Naim <naimtmas@gmail.com>
2022-01-31 19:20:53 +0800

    added dynamic data query and support in projects component

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,10 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import * as styles from "../../styles/projects.module.css"
 import GitHubIcon from "../../assets/github.svg"
 import { FiExternalLink } from "@react-icons/all-files/fi/FiExternalLink"
 
 export default function Projects() {
+  const data = useStaticQuery(graphql`
+    query projectQuery {
+      allMarkdownRemark(
+        filter: {
+          fileAbsolutePath: { regex: "/projects/" }
+          frontmatter: { feature: { eq: true } }
+        }
+      ) {
+        nodes {
+          id
+          frontmatter {
+            title
+            title
+            description
+            githubRepo
+            techStack
+            deployedLink
+          }
+        }
+      }
+    }
+  `)
+  const nodes = data.allMarkdownRemark.nodes
+
   return (
     <section className={styles.projectsContainer}>
       <h1 className={styles.header}>Projects</h1>
@@ -27,93 +51,39 @@ export default function Projects() {
         .
       </p>
       <div className={styles.projectsGrid}>
-        <div className={styles.projectItem}>
-          <div className={styles.projectLinkContainer}>
-            <a
-              href="https://github.com/tengznaim"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <GitHubIcon className={styles.linkIcons}></GitHubIcon>
-            </a>
-            <a
-              href="https://github.com/tengznaim"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <FiExternalLink className={styles.linkIcons}></FiExternalLink>
-            </a>
-            <h2 className={styles.itemText} id={styles.projectTitle}>
-              Project 1
-            </h2>
-            <p className={styles.itemText} id={styles.projectDescription}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
-              culpa eos vitae provident corrupti amet alias fuga impedit magni
-              consequatur.
-            </p>
-            <p className={styles.itemText} id={styles.projectDescription}>
-              Tensorflow Streamlit Pandas
-            </p>
+        {nodes.map(node => (
+          <div className={styles.projectItem} key={node.id}>
+            <div className={styles.projectLinkContainer}>
+              {node.frontmatter.githubRepo && (
+                <a
+                  href={node.frontmatter.githubRepo}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <GitHubIcon className={styles.linkIcons}></GitHubIcon>
+                </a>
+              )}
+              {node.frontmatter.deployedLink && (
+                <a
+                  href={node.frontmatter.deployedLink}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <FiExternalLink className={styles.linkIcons}></FiExternalLink>
+                </a>
+              )}
+              <h2 className={styles.itemText} id={styles.projectTitle}>
+                {node.frontmatter.title}
+              </h2>
+              <p className={styles.itemText} id={styles.projectDescription}>
+                {node.frontmatter.description}
+              </p>
+              <p className={styles.itemText} id={styles.projectDescription}>
+                {node.frontmatter.techStack}
+              </p>
+            </div>
           </div>
-        </div>
-        <div className={styles.projectItem}>
-          <div className={styles.projectLinkContainer}>
-            <a
-              href="https://github.com/tengznaim"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <GitHubIcon className={styles.linkIcons}></GitHubIcon>
-            </a>
-            <a
-              href="https://github.com/tengznaim"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <FiExternalLink className={styles.linkIcons}></FiExternalLink>
-            </a>
-            <h2 className={styles.itemText} id={styles.projectTitle}>
-              Project 1
-            </h2>
-            <p className={styles.itemText} id={styles.projectDescription}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
-              culpa eos vitae provident corrupti amet alias fuga impedit magni
-              consequatur.
-            </p>
-            <p className={styles.itemText} id={styles.projectDescription}>
-              Tensorflow Streamlit Pandas
-            </p>
-          </div>
-        </div>
-        <div className={styles.projectItem}>
-          <div className={styles.projectLinkContainer}>
-            <a
-              href="https://github.com/tengznaim"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <GitHubIcon className={styles.linkIcons}></GitHubIcon>
-            </a>
-            <a
-              href="https://github.com/tengznaim"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <FiExternalLink className={styles.linkIcons}></FiExternalLink>
-            </a>
-            <h2 className={styles.itemText} id={styles.projectTitle}>
-              Project 1
-            </h2>
-            <p className={styles.itemText} id={styles.projectDescription}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
-              culpa eos vitae provident corrupti amet alias fuga impedit magni
-              consequatur.
-            </p>
-            <p className={styles.itemText} id={styles.projectDescription}>
-              Tensorflow Streamlit Pandas
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   )
