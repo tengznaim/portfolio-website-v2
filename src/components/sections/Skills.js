58377a92335569896dc8763b2833fe166be76685
Tengku Naim <naimtmas@gmail.com>
2022-02-01 20:40:40 +0800

    added skills section

---
+++ src/components/sections/Skills.js
@@ -0,0 +1,36 @@
+import React from "react"
+import * as styles from "../../styles/skills.module.css"
+
+export default function Skills() {
+  return (
+    <section className={styles.skillsContainer}>
+      <h1 className={styles.header}>Skills</h1>
+      <table className={styles.skillsTable}>
+        <tr>
+          <td className={styles.categoryCol}>Languages</td>
+          <td className={styles.dataCol}>
+            Python, Java, JavaScript, SQL, HTML, CSS
+          </td>
+        </tr>
+        <tr>
+          <td className={styles.categoryCol}>AI/ML/DS</td>
+          <td className={styles.dataCol}>
+            TensorFlow, PyTorch, scikit-learn, Pandas, Numpy, Matplotlib, OpenCV
+          </td>
+        </tr>
+        <tr>
+          <td className={styles.categoryCol}>Web Development</td>
+          <td className={styles.dataCol}>
+            Flask, React JS, Gatsby JS, Streamlit
+          </td>
+        </tr>
+        <tr>
+          <td className={styles.categoryCol}>Cloud and Ops</td>
+          <td className={styles.dataCol}>
+            Google Cloud Platform, Airflow, Terraform, Docker, Selenium, Git
+          </td>
+        </tr>
+      </table>
+    </section>
+  )
+}
